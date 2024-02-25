4acc365120fbddaa46b2831adc524ed95d34966f
Knightwood <33772264+Knightwood@users.noreply.github.com>
2024-02-25 20:36:52 +0800

    Update build.gradle.kts

--- preference-ui-compose-multi/build.gradle.kts
+++ preference-ui-compose-multi/build.gradle.kts
@@ -1,20 +1,5 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
-/*
- * Copyright 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.ksp)
@@ -167,24 +152,6 @@ afterEvaluate {
                     name.set("ComposePreferenceMultiplatform")
                     description.set("ComposePreferenceMultiplatform")
                     url.set("https://github.com/Knightwood/ComposePreferenceMultiplatform")
-
-//            licenses {
-//                license {
-//                    name.set("MIT")
-//                    url.set("https://opensource.org/licenses/MIT")
-//                }
-//            }
-//            developers {
-//                developer {
-//                    id.set("KaterinaPetrova")
-//                    name.set("Ekaterina Petrova")
-//                    email.set("ekaterina.petrova@jetbrains.com")
-//                }
-//            }
-//            scm {
-//                url.set("https://github.com/KaterinaPetrova/mpp-sample-lib")
-//            }
-
                 }
             }
             create<MavenPublication>("release") {
